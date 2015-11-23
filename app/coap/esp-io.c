a5083d870b39ca98e3aceeff1a1b140396e542e9
jollen <jollen@moko365.com>
2015-11-23 21:01:48 +0800

    add: low-level coap io for ESP8266, #1

---
+++ app/coap/esp-io.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015, WoT.City Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/**
+ * \file
+ *      Helper functions for ESP8266 connections I/O
+ * \author
+ *      Jollen <jollen@jollen.org>
+ */
+
+#include "c_stdio.h"
+#include "c_stdlib.h"
+#include "c_string.h"
+#include "c_ctype.h"
+
+#include "er-coap-13.h"
+#include "er-coap-13-transactions.h"
+
+#include "espconn.h"
+#include "lwip/app/espconn.h"
+
+#define DEBUG 0
+#if DEBUG
+#define PRINTF(...) c_printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+
+/**
+ * Send UDP packet over ESP8266 connection.
+ *
+ * @param t The coap_transaction_t instance
+ * @param data The paylaod to be sent.
+ * @param length The length of payload.
+ *
+ * @return -1 if error occured, 0 if success
+ */
+int coap_send_message(coap_transaction_t *t, uint8_t *data, uint16_t length) 
+{
+  struct espconn *espconn = (struct espconn *)t->private;
+  struct espconn_msg *pnode = NULL;
+  bool value = false;
+   
+  if (espconn == NULL) {
+      return ESPCONN_ARG;
+  }
+
+  espconn->state = ESPCONN_WRITE;
+
+  value = espconn_find_connection(espconn, &pnode);
+  PRINTF("espconn_find_connection = %d\n", value);
+
+  if (value)
+    espconn_udp_sent(pnode, data, length);
+  else
+    return ESPCONN_ARG;
+
+  return ESPCONN_OK;
+}
